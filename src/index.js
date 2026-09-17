22d61d033564f72dd6b4a05d18522179606b4559
renatomenendes <89153259+renatomenendes@users.noreply.github.com>
2026-09-17 13:35:06 -0300

    feat: expose profiling API surface

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { analyzeProject } from './engine/analyze.js';
+export { buildProfile, PROFILE_SCHEMA_VERSION } from './profile/build.js';
+export { buildHealthProfile } from './profile/health.js';
+export { extractPageMetadata } from './profile/pages.js';
+export { buildUsageProfile } from './profile/usage.js';
